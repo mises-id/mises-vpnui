e1b35ce5dafb1300bd79ffbd4858744ed9700639
suyankai1984 <suyankai1984@163.com>
2024-06-02 11:56:31 +0800

    debug

--- src/components/Web3Provider/index.tsx
+++ src/components/Web3Provider/index.tsx
@@ -1,10 +1,11 @@
 import { Web3ReactHooks, Web3ReactProvider } from '@web3-react/core'
 import { ReactNode } from 'react'
-import type { MetaMask } from '@web3-react/metamask'
-import { hooks as metaMaskHooks, metaMask } from './metamask'
+//import type { MetaMask } from '@web3-react/metamask'
+import { hooks as misesWalletHooks, misesWallet } from './mises'
+import { MisesWallet } from './misesWallet'
 
-const connectors: [MetaMask, Web3ReactHooks][] = [
-  [metaMask, metaMaskHooks],
+const connectors: [MisesWallet, Web3ReactHooks][] = [
+  [misesWallet, misesWalletHooks],
 ]
 export default function Web3Provider({ children }: { children: ReactNode }) {
   return (
